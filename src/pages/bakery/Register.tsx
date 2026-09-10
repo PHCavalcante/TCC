a7038fcb2cfde1c70235ab81e0f56c827ba6df9c
PHCavalcante <pedhcavdsan1603@gmail.com>
2026-09-10 10:50:08 -0300

    refactor: small changes in bakery auth pages

--- src/pages/bakery/Register.tsx
+++ src/pages/bakery/Register.tsx
@@ -1,20 +1,20 @@
 import logo from "../../assets/logo.png";
 import { palette } from "../../theme";
-import { Link } from "@tanstack/react-router"
+import { Link } from "@tanstack/react-router";
 
-export default function Login() {
+export default function Register() {
   return (
-     <div className="flex flex-col h-full items-center px-9.5">
+    <div className="flex flex-col h-full items-center px-9.5">
       <div className="flex flex-col mt-9.5 items-center">
         <img src={logo} alt="Logo" className="select-none" draggable={false} />
         <span className="text-xl" style={{ color: palette.light.accent }}>
           {"Placeholder"}
         </span>
       </div>
-      <h1 className="text-2xl font-bold mt-10">Entrar</h1>
+      <h1 className="text-2xl font-bold mt-10">Registre sua padaria</h1>
       <div className="flex flex-col gap-4 mt-6">
         <div className="flex flex-col">
-          <label htmlFor="email">Email/CNPJ</label>
+          <label htmlFor="email">Email</label>
           <input
             id="email"
             name="email"
@@ -31,17 +31,26 @@ export default function Login() {
             className="w-80 h-12 rounded-lg border border-black px-4 focus:outline-none focus:ring-2"
           />
         </div>
+        <div className="flex flex-col">
+          <label htmlFor="confirmPassword">Confirmar Senha</label>
+          <input
+            id="confirmPassword"
+            name="confirmPassword"
+            type="password"
+            className="w-80 h-12 rounded-lg border border-black px-4 focus:outline-none focus:ring-2"
+          />
+        </div>
       </div>
       <button
         className="w-80 h-12 text-white rounded-lg mt-6 hover:bg-blue-600 transition-colors"
         style={{ backgroundColor: palette.light.accent }}
       >
-        Entrar
+        Criar Conta
       </button>
       <div className="mt-4 text-sm py-10">
-        <span className="text-gray-600">Padaria não registrada? </span>
-        <Link to={"/bakery/register"} viewTransition>Registre-se</Link>
+        <span className="text-gray-600">Já possui conta? </span>
+        <Link to={"/bakery/login"} viewTransition>Faça Login</Link>
       </div>
     </div>
-);
+  );
 }
