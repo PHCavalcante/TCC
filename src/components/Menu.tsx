c455d3f4c15374cc0790dadf8b885391c31cf53d
PHCavalcante <pedhcavdsan1603@gmail.com>
2026-09-25 09:18:38 -0300

    refactor: melhorias visuais no componente Menu e na tela de catálago

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,6 @@
 import { palette } from "../theme";
 import Logo from "../assets/logo.png";
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import dashboardLogo from "../assets/dashboard.svg"
 import ordersLogo from "../assets/orders.svg";
 import exit from "../assets/exit.svg";
@@ -8,39 +8,42 @@ import { useLocation } from "@tanstack/react-router";
 import manager from "../assets/manager.svg";
 import catalog from "../assets/catalog.svg";
 
-function logOut() {
-    if (confirm("Você tem certeza que deseja sair?")) {
-        // implementar a lógica de logout
-    }
-}
-
 export default function Menu() {
     const location = useLocation();
+    const navigate = useNavigate();
+    
+    function logOut() {
+        if (confirm("Você tem certeza que deseja sair?")) {
+            // implementar a lógica de logout
+            navigate({ to: "/bakery/login", replace: true})
+        }
+    }
+
     return (
         <div style={{
             backgroundColor: palette.light.accent
         }}
         className="flex min-w-76.5 h-screen flex-col items-center shadow-[4px_0px_4px_0px_#00000055]"
         >
-        <img src={Logo} alt="Logo" className="mt-4" />
+        <img src={Logo} alt="Logo" width={150} className="mt-4" draggable={false} />
         <span className="text-xl text-white">{"Placeholder"}</span>
         <div className="mt-20">
-            <div style={{ backgroundColor: location.pathname === "/bakery/dashboard" ? palette.light.primary : "transparent" }} className="flex w-61.5 items-center gap-3.5 text-white text-[20px] font-bold px-4 py-2 rounded-lg mb-4">
+            <Link to={"/bakery/dashboard"} viewTransition style={{ backgroundColor: location.pathname === "/bakery/dashboard" ? palette.light.primary : "transparent" }} className="flex w-61.5 items-center gap-3.5 text-white text-[20px] font-bold px-4 py-2 rounded-lg mb-4">
                 <img src={dashboardLogo} alt="Dashboard"/>
-                <Link to={"/bakery/dashboard"} viewTransition>Dashboard</Link>
-            </div>
-            <div style={{ backgroundColor: location.pathname === "/bakery/orders" ? palette.light.primary : "transparent" }} className="flex items-center gap-3.5 text-white text-[20px] font-bold px-4 py-2 rounded-lg mb-4">
+                Dashboard
+            </Link>
+            <Link to={"/bakery/orders"} viewTransition style={{ backgroundColor: location.pathname === "/bakery/orders" ? palette.light.primary : "transparent" }} className="flex items-center gap-3.5 text-white text-[20px] font-bold px-4 py-2 rounded-lg mb-4">
                 <img src={ordersLogo} alt="Pedidos"/>
-                <Link to={"/bakery/orders"} viewTransition>Pedidos</Link>
-            </div>
-              <div style={{ backgroundColor: location.pathname === "/bakery/management" ? palette.light.primary : "transparent" }} className="flex items-center gap-3.5 text-white text-[20px] font-bold px-4 py-2 rounded-lg mb-4">
+                Pedidos
+            </Link>
+              <Link to={"/bakery/management"} viewTransition style={{ backgroundColor: location.pathname === "/bakery/management" ? palette.light.primary : "transparent" }} className="flex items-center gap-3.5 text-white text-[20px] font-bold px-4 py-2 rounded-lg mb-4">
                 <img src={manager} alt="Gerência"/>
-                <Link to={"/bakery/management"} viewTransition>Gerência</Link>
-            </div>
-              <div style={{ backgroundColor: location.pathname === "/bakery/catalog" ? palette.light.primary : "transparent" }} className="flex items-center gap-3.5 text-white text-[20px] font-bold px-4 py-2 rounded-lg mb-4">
+                Gerência
+            </Link>
+              <Link to={"/bakery/catalog"} viewTransition style={{ backgroundColor: location.pathname === "/bakery/catalog" ? palette.light.primary : "transparent" }} className="flex items-center gap-3.5 text-white text-[20px] font-bold px-4 py-2 rounded-lg mb-4">
                 <img src={catalog} alt="Catálogo"/>
-                <Link to={"/bakery/catalog"} viewTransition>Catálogo</Link>
-            </div>
+                Catálogo
+            </Link>
         </div>
         <button onClick={logOut} className="mt-auto mb-4 cursor-pointer flex items-center gap-3.5 text-white text-[20px] font-bold px-4 py-2 rounded-lg">
             <img src={exit} alt="botão sair" />    
--- src/pages/bakery/Catalog.tsx
+++ src/pages/bakery/Catalog.tsx
@@ -3,17 +3,26 @@ import { palette } from "../../theme";
 import add from "../../assets/add.svg";
 
 export default function Catalog() {
+    const items = []; // placeholder
     return (
         <div className="flex">
             <Menu />
             <div className="flex w-full flex-col ml-14.75 mr-14.75">
                 <div className="flex mt-11.25 mb-24 items-center justify-between">
                     <h1 className="font-bold text-3xl" style={{ color: palette.light.accent }}>Catálogo</h1>
-                    <button className="flex gap-2.5 items-center text-white font-bold py-2 px-4 rounded-lg cursor-pointer" style={{ backgroundColor: palette.light.accent}}>
+                    {items.length > 0 && <button className="flex gap-2.5 items-center text-white font-bold py-2 px-4 rounded-lg cursor-pointer shadow-md" style={{ backgroundColor: palette.light.accent }}>
+                        <img src={add} alt="Adicionar Item" />
+                        Adicionar Item
+                    </button>}
+                </div>
+                {items.length === 0 && <div className="flex flex-col items-center gap-2.5">
+                    <p className="text-center text-xl font-bold mt-50">Nenhum item no catálogo.</p>
+                    <p className="text-center text-lg">O estabelecimento ficará indisponível até que itens sejam adicionados.</p>
+                    <button className="flex gap-2.5 items-center text-white font-bold py-2 px-4 rounded-lg cursor-pointer shadow-md" style={{ backgroundColor: palette.light.accent }}>
                         <img src={add} alt="Adicionar Item" />
                         Adicionar Item
                     </button>
-                </div>
+                </div>}
             </div>
         </div>
     );
